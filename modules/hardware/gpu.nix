9b8a983997588b88204b4783c01c4512b50933c9
j4v3l <jj4v3l@gmail.com>
2025-12-04 17:36:42 -0500

    Enhance GPU configuration by adding nvidia-container-runtime wrapper to system packages

--- modules/hardware/gpu.nix
+++ modules/hardware/gpu.nix
@@ -38,7 +38,12 @@ in {
         graphics.extraPackages = with pkgs; [nvidia-vaapi-driver];
         nvidia-container-toolkit.enable = mkDefault true;
       };
-      environment.systemPackages = with pkgs; [nvidia-container-toolkit];
+      environment.systemPackages = with pkgs; [
+        nvidia-container-toolkit
+        (writeShellScriptBin "nvidia-container-runtime" ''
+          exec ${nvidia-container-toolkit}/bin/nvidia-ctk runtime "$@"
+        '')
+      ];
     })
 
     (mkIf isIntel {
