bc333622446ed778b113aa63a6510b414f352bdd
Javel Palmer <37094748+j4v3l@users.noreply.github.com>
2026-01-10 01:50:47 -0500

    feat: add additional GPU device options and environment variables in containers.nix

--- hosts/avalanche/containers.nix
+++ hosts/avalanche/containers.nix
@@ -11,6 +11,12 @@ _: {
     extraOptions = [
       "--runtime=nvidia"
       "--gpus=all"
+      "--device=/dev/nvidiactl"
+      "--device=/dev/nvidia0"
+      "--device=/dev/nvidia-uvm"
+      "--device=/dev/nvidia-uvm-tools"
+      "--env=NVIDIA_VISIBLE_DEVICES=all"
+      "--env=NVIDIA_DRIVER_CAPABILITIES=compute,utility,video,graphics"
       "--init"
       "--pid=host"
     ];
