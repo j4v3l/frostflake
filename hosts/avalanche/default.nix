09fe083ba63d799fe0967493f0800f1bda762530
Javel Palmer <37094748+j4v3l@users.noreply.github.com>
2026-01-10 01:43:25 -0500

    feat: add jager password secret and update lastmodified timestamp in avalanche.yaml

--- hosts/avalanche/default.nix
+++ hosts/avalanche/default.nix
@@ -59,6 +59,18 @@ in
         users.jager.openssh.authorizedKeys.keys = [
           "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHCsQ4NNDuuAj/NLrC9yXVoGRNU5DRTEqC2ybN+Y9Qjf jager@Javels-MacBook-Pro.local"
         ];
+        users.jager.hashedPasswordFile = "/run/secrets/jager-password";
+      };
+
+      frostflake.secrets.extraSecrets = {
+        "jager-password" = {
+          sopsFile = frostflakeRoot + "/secrets/hosts/avalanche.yaml";
+          key = "jager_password_hash";
+          path = "/run/secrets/jager-password";
+          mode = "0400";
+          owner = "root";
+          group = "root";
+        };
       };
     };
   }
